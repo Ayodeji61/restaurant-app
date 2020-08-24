28ebd7e1d63a00616685eb649401a6f5a179e02b
Ayodeji61 <alabidejimichael@gmail.com>
2020-08-24 10:08:40 +0100

    navbar added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { HomeHeader } from '../utils';
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" keywords={[ `gatsby`, `application` ]} />
 		<h3>this is the home page</h3>
+		<HomeHeader />
 	</Layout>
 );
 
---
+++ src/utils/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function HomeHeader(img, children) {
+	return <IndexHeader />;
+}
+
+const IndexHeader = styled.div`
+min-height: calc(100vh - 68px)
+background:  
+
+`;
+
+export { HomeHeader };
